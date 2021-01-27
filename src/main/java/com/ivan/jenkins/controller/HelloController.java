c2c64a3d3fa67a59cdfb2985584d8b63f83330a5
Ivanyany <754339948@qq.com>
2021-01-27 15:49:59 +0800

    测试代码

---
+++ src/main/java/com/ivan/jenkins/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.ivan.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ivan
+ * @Title: HelloController
+ * @date 2021/1/2715:36
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, This is Test For Jenkins.";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Ivan.";
+    }
+}
